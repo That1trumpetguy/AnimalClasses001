88342274ee778d6d44512f680bf22fa4a82105fd
khadomills <MillerNathan@cwu.edu>
2023-04-14 08:52:09 -0700

    Added Goat.java class - Nathan Miller

---
+++ Goat.java
@@ -0,0 +1,14 @@
+public class Goat extends Animal {
+    public void play() {
+        System.out.println("The goat frolics in the field");
+    }
+
+    public void sound() {
+        System.out.println("The goat lets out a bloodcurdling scream");
+    }
+
+    public Goat(String name) {
+        super(name);
+
+    }
+}
